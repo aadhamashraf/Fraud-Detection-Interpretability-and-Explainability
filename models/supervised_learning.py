fb52da748e2ee8148d0c63e761377048108a06e2
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2025-04-12 11:31:35 +0200

    Update supervised_learning.py

--- models/supervised_learning.py
+++ models/supervised_learning.py
@@ -1,1 +1,7 @@
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier

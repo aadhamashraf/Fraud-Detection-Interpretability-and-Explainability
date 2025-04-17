5e7ee0635dc8bc78233199fe59cb7637aa1b28ec
Adham Ashraf Eltholth <147609804+aadhamashraf@users.noreply.github.com>
2025-04-17 18:34:41 +0200

    Update adham_models.py

--- models/adham_models.py
+++ models/adham_models.py
@@ -172,7 +172,7 @@ def main(df, pop=10, gen=10, mrate=0.1):
     
     # train_rf(X_train_resampled, X_test, y_train_resampled, y_test, selected_features)
     model, selected_features = train_nb(X_train_resampled, X_test, y_train_resampled, y_test, selected_features) 
-    selected_indices = [X_test.columns.get_loc(col) for col in selected_features]
+    selected_indices = [i for i in selected_features]
 
     ## **Explainability  Section**
     explain_lime(X_test, model, selected_features, selected_indices, idx=0)
